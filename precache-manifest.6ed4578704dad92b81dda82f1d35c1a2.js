c3a3fa0613a1403ad77aeace92ab58866c8a1388
Andrei Racasan <aracasan@outlook.com>
2020-10-09 22:34:18 +0100

    Updates

---
+++ precache-manifest.6ed4578704dad92b81dda82f1d35c1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bc8df524e993b5c5bc9c621406faf1",
+    "url": "/markdown_machine/index.html"
+  },
+  {
+    "revision": "df80c77bb28b03d522bc",
+    "url": "/markdown_machine/static/css/main.d02a9c4b.chunk.css"
+  },
+  {
+    "revision": "e8e1790cc1e6ef9814eb",
+    "url": "/markdown_machine/static/js/2.5c1705a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown_machine/static/js/2.5c1705a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df80c77bb28b03d522bc",
+    "url": "/markdown_machine/static/js/main.aa5eab87.chunk.js"
+  },
+  {
+    "revision": "5f713e920e8f684ba02d",
+    "url": "/markdown_machine/static/js/runtime-main.929bdf3d.js"
+  }
+]);
